2ce10958d14b99f613bf799fe84d941baf590ca8
nicoweiler <nicoweiler@gmx.de>
2019-05-08 16:09:35 +0200

    create Common and ServersideSettings

---
+++ SharedShoppingList/src/sharedShoppingList/shared/CommonSettings.java
@@ -0,0 +1,20 @@
+package sharedShoppingList.shared;
+
+/**
+ * Zentrale Konstante, die sowohl Server- als auch Client-seitig benötigt
+ * werden. Superklasse von <code>ClientsideSettings</code> und
+ * <code>ServersideSettings</code>.
+ * 
+ * @author Nico Weiler
+ * @version 1.0
+ */
+
+public class CommonSettings {
+	
+	 /**
+     * Default constructor
+     */
+    public CommonSettings() {
+    }
+
+}
