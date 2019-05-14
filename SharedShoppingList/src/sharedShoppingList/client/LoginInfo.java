c38f4925257930474431afd19a67a715337080e2
nicolaifischbach <nicolai.fischbach@gmx.de>
2019-05-14 14:52:57 +0200

    Edit class SharedShoppingListEditorEntry Add further classes and interfaces

---
+++ SharedShoppingList/src/sharedShoppingList/client/LoginInfo.java
@@ -0,0 +1,62 @@
+package sharedShoppingList.client;
+
+
+import java.io.Serializable;
+
+public class LoginInfo implements Serializable {
+
+	
+	private boolean loggedIn = false;
+	  private String loginUrl;
+	  private String logoutUrl;
+	  private String emailAddress;
+	  private String nickname;
+
+
+	  /*
+	   * @return loggedIn Information ob, der User eingeloggt ist oder nicht.
+	   */
+	  public boolean isLoggedIn() {
+	    return loggedIn;
+	  }
+
+	  /*
+	   *  * @parameter loggedIn setzt den Login Status.
+	   */
+	  public void setLoggedIn(boolean loggedIn) {
+	    this.loggedIn = loggedIn;
+	  }
+
+
+	  public String getLoginUrl() {
+	    return loginUrl;
+	  }
+
+	  public void setLoginUrl(String loginUrl) {
+	    this.loginUrl = loginUrl;
+	  }
+
+	  public String getLogoutUrl() {
+	    return logoutUrl;
+	  }
+
+	  public void setLogoutUrl(String logoutUrl) {
+	    this.logoutUrl = logoutUrl;
+	  }
+
+	  public String getEmailAddress() {
+	    return emailAddress;
+	  }
+
+	  public void setEmailAddress(String emailAddress) {
+	    this.emailAddress = emailAddress;
+	  }
+
+	  public String getNickname() {
+	    return nickname;
+	  }
+
+	  public void setNickname(String nickname) {
+	    this.nickname = nickname;
+	  }
+	}
--- SharedShoppingList/src/sharedShoppingList/client/LoginService.java
+++ SharedShoppingList/src/sharedShoppingList/client/LoginService.java
@@ -1,5 +1,9 @@
 package sharedShoppingList.client;
 
-public class LoginService {
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
+@RemoteServiceRelativePath("login")
+public interface LoginService extends RemoteService {
+  public LoginInfo login(String requestUri);
 }
