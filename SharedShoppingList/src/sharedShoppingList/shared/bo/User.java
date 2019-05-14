badacb9d01447d6cc6a6bcaa963eed3f119a4fd8
TobiasIlg <ti009@hdm-stuttgart.de>
2019-05-14 14:56:22 +0200

    User erweitert

--- SharedShoppingList/src/sharedShoppingList/shared/bo/User.java
+++ SharedShoppingList/src/sharedShoppingList/shared/bo/User.java
@@ -10,10 +10,34 @@ import java.sql.Timestamp;
 public class User extends BusinessObject{
 	
 	private int id;
+	private int groupid;
+	private String username;
 	private String name;
+	private String gmail;
 	
 	public User() {
 		super();	
+	}
+	
+	public int getGroupId() {
+		return groupid;
+	}
+	public void setGroupId(int groupid) {
+		this.groupid = groupid;
+	}	
+	
+	public String getUserName() {
+		return username;
+	}
+	public void setUserName(String username) {
+		this.username = username;
+	}	
+	
+	public String getGmail() {
+		return gmail;
+	}
+	public void setGmail(String gmail) {
+		this.gmail = gmail;
 	}
 
 }
