140b8e8e4a0981477626c09e7d97aca29fdf09c1
leonseiz <48949630+leonseiz@users.noreply.github.com>
2019-05-14 15:08:57 +0200

    Create structure for Editor

---
+++ SharedShoppingList/src/sharedShoppingList/server/EinkaufslistenverwaltungImpl.java
@@ -0,0 +1,10 @@
+package sharedShoppingList.server;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+import sharedShoppingList.shared.Einkaufslistenverwaltung;
+
+public class EinkaufslistenverwaltungImpl extends RemoteServiceServlet implements Einkaufslistenverwaltung{
+	
+
+}
---
+++ SharedShoppingList/src/sharedShoppingList/shared/EinkaufslistenverwaltungAsync.java
@@ -0,0 +1,9 @@
+package sharedShoppingList.shared;
+
+/*
+ * @author: Leon Seiz, Nico Weiler, Tobias Ilg
+ */
+
+public interface EinkaufslistenverwaltungAsync {
+
+}
