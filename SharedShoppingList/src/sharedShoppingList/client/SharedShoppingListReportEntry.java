aed580cad4dbbc86aa3419b7b439b8a7cd11be91
MoritzHampe <moritzhampe124@web.de>
2019-05-21 13:43:52 +0200

    Löschen der Greeting Klassen und hinzufügen der ReportEntry klasse

---
+++ SharedShoppingList/src/sharedShoppingList/client/SharedShoppingListReportEntry.java
@@ -0,0 +1,17 @@
+package sharedShoppingList.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/*Die Klasse <code>ReportGenerator</code> dient zur Zusammensetzung 
+ * aller Elemente des Reports 
+ */
+
+public class SharedShoppingListReportEntry implements EntryPoint{
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
