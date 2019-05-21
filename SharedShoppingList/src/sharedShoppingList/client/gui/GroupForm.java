3939301389dff7805a50895572232e3e594746b9
patrick <treiber_patrick@web.de>
2019-05-21 14:53:33 +0200

    create class StoreForm

--- SharedShoppingList/src/sharedShoppingList/client/gui/GroupForm.java
+++ SharedShoppingList/src/sharedShoppingList/client/gui/GroupForm.java
@@ -1,10 +1,16 @@
 
 package sharedShoppingList.client.gui;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+
+
 
 
 
@@ -14,14 +20,72 @@ import com.google.gwt.user.client.ui.VerticalPanel;
  *
  */
 
-public class GroupForm extends VerticalPanel{
+public class GroupForm extends AbstractAdministrationForm {
 
-	Label groupNameLabel = new Label("Name der Gruppe: ");
-	Label enterUserNameLabel = new Label("Benutzername eingeben:");
-	Label foundUserLabel = new Label ("Gefundene Benutzer");
-	Label addUserLabel = new Label ("Hinzugef√ºgter User:");
-	TextBox nameTextBox = new TextBox();
-	
-	Button addGroupButton = new Button("Gruppe mit genannten Usern anlegen");
+
+		@Override
+		protected String nameForm() {
+
+			return "Meine Lieblingsartikel<3";
+		}
+
+		// Konstruktor
+		public GroupForm() {
+
+			saveButton.addClickHandler(new CreateStoreHandler());
+			cancelButton.addClickHandler(new CancelClickHandler());
+			addButton.addClickHandler(new AddFavArticleClickHandler());
+
+		}
+
+		/**
+		 * Hiermit wird der Erstellvorgang eines neuen Favoritens abbgebrochen.
+		 */
+		private class CancelClickHandler implements ClickHandler {
+
+			public void onClick(ClickEvent event) {
+				RootPanel.get("Details").clear();
+
+			}
+
+		}
+
+		/**
+		 * Sobald das Textfeld ausgef¸llt wurde, wird ein neuer Favorite nach
+		 * dem Klicken des Best‰tigungsbutton erstellt.
+		 */
+		private class CreateStoreHandler implements ClickHandler {
+
+			public void onClick(ClickEvent event) {
+
+			}
+		}
+
+		/**
+		 * Sobald das Textfeld ausgef¸llt wurde, wird ein neuer Favorit nach dem Klicken
+		 * des addButton erstellt.
+		 */
+		private class AddFavArticleClickHandler implements ClickHandler {
+
+			public void onClick(ClickEvent event) {
+
+			}
+		}
+
+		/**
+		 * Callback wird benˆtigt, um den Store zu erstellen
+		 */
+		private class FavArticleCreationCallback implements AsyncCallback<Void> {
+
+			@Override
+			public void onFailure(Throwable caught) {
+				Notification.show("Der Store konnte nicht erstellt werden");
+			}
+
+			@Override
+			public void onSuccess(Void event) {
+				Notification.show("Der Store wurde erfolgreich erstellt");
+			}
+		}
 }
 
