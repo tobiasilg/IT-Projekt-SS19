50a9e9eb581ae057e5fd26c30ee62cad5f392a5a
MoritzHampe <moritzhampe124@web.de>
2019-05-21 13:04:46 +0200

    Auskommentieren des Login-Codes in der Entry Klasse + hinzufügen der Nav

---
+++ SharedShoppingList/src/sharedShoppingList/client/gui/Navigator.java
@@ -0,0 +1,9 @@
+package sharedShoppingList.client.gui;
+
+public class Navigator {
+	/*
+	 * Bildet die Navigationsleiste zum anzeigen und selektieren der Einkaufsliste, 
+	 * welche der Gruppe untergeordnet ist.
+	 */
+
+}
