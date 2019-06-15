514ff3473d0d7e0c67dcf691289f6df669be3cff
patrick <treiber_patrick@web.de>
2019-06-15 19:00:15 +0200

    Style Report and fixed BUg

--- SharedShoppingList/src/sharedShoppingList/client/reportgui/HeaderRepo.java
+++ SharedShoppingList/src/sharedShoppingList/client/reportgui/HeaderRepo.java
@@ -1,5 +1,6 @@
 package sharedShoppingList.client.reportgui;
 
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -10,7 +11,9 @@ public class HeaderRepo extends VerticalPanel {
 
 	// Erstellung der Header Divs
 	private Label label = new Label("KEKWAY-REPORT"); // Label der den Namen im Header anzeigt
+	private Label headerSubtitle = new Label("share it - buy it");
 	private HorizontalPanel hp = new HorizontalPanel();
+	private Button testButton = new Button("drück mich");
 
 	public HeaderRepo() {
 		super();
@@ -29,10 +32,14 @@ public class HeaderRepo extends VerticalPanel {
 		/*
 		 * Vergeben von Stylenamen, um die Divs über CSS ansprechen zu können
 		 */
-
+		label.addStyleName("header_title");
+		headerSubtitle.addStyleName("header_subtitle");
 		// Gesamtes Header Div
 		this.addStyleName("header");
+
 		hp.add(label);
+		hp.add(headerSubtitle);
+		hp.add(testButton);
 		this.add(hp);
 
 	}
