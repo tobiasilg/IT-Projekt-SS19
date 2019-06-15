531e6239172c9081148deb64762c70bcffd4d390
patrick <treiber_patrick@web.de>
2019-06-15 17:47:56 +0200

    opening the generator made possible

--- SharedShoppingList/src/sharedShoppingList/server/report/ReportClientImpl.java
+++ SharedShoppingList/src/sharedShoppingList/server/report/ReportClientImpl.java
@@ -3,7 +3,6 @@ package sharedShoppingList.server.report;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
-import java.util.Vector;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
@@ -34,68 +33,67 @@ import sharedShoppingList.shared.report.SimpleParagraph;
  */
 
 public class ReportClientImpl extends RemoteServiceServlet implements ReportClient {
-	
+
 	/**
-	 * Der ReportClient benötigt Zugriff auf die ELV,
-	 * da dort wichtige Methoden für die Koexistenz von Datenobjekten enthalten sind.
+	 * Der ReportClient benötigt Zugriff auf die ELV, da dort wichtige Methoden für
+	 * die Koexistenz von Datenobjekten enthalten sind.
 	 */
-	
+
 	private Einkaufslistenverwaltung elv;
 
-	
-	
-	/** OriginalKommentar
-     * <p>
-     * Ein <code>RemoteServiceServlet</code> wird unter GWT mittels
-     * <code>GWT.create(Klassenname.class)</code> Client-seitig erzeugt. Hierzu
-     * ist ein solcher No-Argument-Konstruktor anzulegen. Ein Aufruf eines anderen
-     * Konstruktors ist durch die Client-seitige Instantiierung durch
-     * <code>GWT.create(Klassenname.class)</code> nach derzeitigem Stand nicht
-     * möglich.
-     * </p>
-     * <p>
-     * Es bietet sich also an, eine separate Instanzenmethode zu erstellen, die
-     * Client-seitig direkt nach <code>GWT.create(Klassenname.class)</code>
-     * aufgerufen wird, um eine Initialisierung der Instanz vorzunehmen.
-     * </p>
-     */
+	/**
+	 * OriginalKommentar
+	 * <p>
+	 * Ein <code>RemoteServiceServlet</code> wird unter GWT mittels
+	 * <code>GWT.create(Klassenname.class)</code> Client-seitig erzeugt. Hierzu ist
+	 * ein solcher No-Argument-Konstruktor anzulegen. Ein Aufruf eines anderen
+	 * Konstruktors ist durch die Client-seitige Instantiierung durch
+	 * <code>GWT.create(Klassenname.class)</code> nach derzeitigem Stand nicht
+	 * möglich.
+	 * </p>
+	 * <p>
+	 * Es bietet sich also an, eine separate Instanzenmethode zu erstellen, die
+	 * Client-seitig direkt nach <code>GWT.create(Klassenname.class)</code>
+	 * aufgerufen wird, um eine Initialisierung der Instanz vorzunehmen.
+	 * </p>
+	 */
 
 	public ReportClientImpl() throws IllegalArgumentException {
-		
+
 	}
-	
+
 	/**
-     * Initialsierungsmethode. Siehe dazu Anmerkungen zum No-Argument-Konstruktor.
-     * 
-     * @see ReportClientImpl
-     */
-	
-	public void init()throws IllegalArgumentException {
-		
-		  /*
-         * Ein ReportClientImpl-Objekt instantiiert für seinen Eigenbedarf eine
-         * EinkaufslistenverwaltungImpl-Instanz.
-         */
-    	 EinkaufslistenverwaltungImpl e = new EinkaufslistenverwaltungImpl();
-    	    e.init();
-    	    this.elv = e;
-		
+	 * Initialsierungsmethode. Siehe dazu Anmerkungen zum No-Argument-Konstruktor.
+	 * 
+	 * @see ReportClientImpl
+	 */
+
+	public void init() throws IllegalArgumentException {
+
+		/*
+		 * Ein ReportClientImpl-Objekt instantiiert für seinen Eigenbedarf eine
+		 * EinkaufslistenverwaltungImpl-Instanz.
+		 */
+		EinkaufslistenverwaltungImpl e = new EinkaufslistenverwaltungImpl();
+		e.init();
+		this.elv = e;
+
 	}
-	
-	   /**
-     * Auslesen der zugehörigen Einkaufslistenverwaaltung (interner Gebrauch).
-     * 
-     * @return das elv Objekt
-     */
-    protected Einkaufslistenverwaltung getEinkaufslistenverwaltung() {
-    
-    	return this.elv;
-    }
-    
+
+	/**
+	 * Auslesen der zugehörigen Einkaufslistenverwaaltung (interner Gebrauch).
+	 * 
+	 * @return das elv Objekt
+	 */
+	protected Einkaufslistenverwaltung getEinkaufslistenverwaltung() {
+
+		return this.elv;
+	}
+
 	@Override
 	public void setArticle(Article a) throws IllegalArgumentException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -115,88 +113,81 @@ public class ReportClientImpl extends RemoteServiceServlet implements ReportClie
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	/**
 	 * Report, der alle Listeneinträge ausgibt, je nachdem, was Selektiert wird.
+	 * 
 	 * @see ListEntryMapper.findByStoreAndDate --> Hier befinden sich die jeweiligen
-	 * SQL Statements
-	 * @see sharedShoppingList.shared.ReportClient#createListByPeriodAndStore(sharedShoppingList.shared.bo.Store, java.sql.Timestamp)
+	 *      SQL Statements
+	 * @see sharedShoppingList.shared.ReportClient#createListByPeriodAndStore(sharedShoppingList.shared.bo.Store,
+	 *      java.sql.Timestamp)
 	 */
 
 	@Override
 	public AllListEntriesByStoreAndPeriod createListByPeriodAndStore(Store store, Timestamp beginningDate) {
-		
-		  
-    	if(this.getEinkaufslistenverwaltung() == null) {
-    		return null;
-    		}
-		
+
+		if (this.getEinkaufslistenverwaltung() == null) {
+			return null;
+		}
+
 		List<ListEntry> listEntries = elv.getEntriesByStoreAndDate(store, beginningDate);
 		AllListEntriesByStoreAndPeriod result = new AllListEntriesByStoreAndPeriod();
-		
-		
+
 		/**
 		 * Titel des Reports
 		 */
-		
+
 		result.setTitle("Report:");
-		
+
 		/**
 		 * Erstellungsdatum wird gesetzt
 		 */
-		
+
 		result.setCreated(new Date());
-		
-		   /*
-	     * Ab hier erfolgt die Zusammenstellung der Kopfdaten (die Dinge, die oben
-	     * auf dem Report stehen) des Reports. Die Kopfdaten sind mehrzeilig, daher
-	     * die Verwendung von CompositeParagraph.
-	     */
-	    CompositeParagraph header = new CompositeParagraph();
 
-	    // Name des Händlers aufnehmen
-	    header.addSubParagraph(new SimpleParagraph(store.getName()));
+		/*
+		 * Ab hier erfolgt die Zusammenstellung der Kopfdaten (die Dinge, die oben auf
+		 * dem Report stehen) des Reports. Die Kopfdaten sind mehrzeilig, daher die
+		 * Verwendung von CompositeParagraph.
+		 */
+		CompositeParagraph header = new CompositeParagraph();
 
-	    // StoreID aufnehmen
-	    header.addSubParagraph(new SimpleParagraph("Store-ID.: " + store.getId()));
-	    
-	    //Zeitraum anzeigen lassen
-	    header.addSubParagraph(new SimpleParagraph("Zeitraum: Von " + beginningDate.getDate() + "bis heute"));
-	    
-	    
+		// Name des Händlers aufnehmen
+		header.addSubParagraph(new SimpleParagraph(store.getName()));
 
-	    // Hinzufügen der zusammengestellten Kopfdaten zu dem Report
-	    result.setHeaderData(header);
+		// StoreID aufnehmen
+		header.addSubParagraph(new SimpleParagraph("Store-ID.: " + store.getId()));
 
-	   
-	    for (ListEntry le : listEntries) {
-	    	
-	      // Eine leere Zeile anlegen.
-	    	Row entryRow=new Row();
+		// Zeitraum anzeigen lassen
+		header.addSubParagraph(new SimpleParagraph("Zeitraum: Von " + beginningDate.getDate() + "bis heute"));
 
-	      // Erste Spalte: ListEntry ID
-	      entryRow.addColumn(new Column(String.valueOf(le.getId())));
+		// Hinzufügen der zusammengestellten Kopfdaten zu dem Report
+		result.setHeaderData(header);
 
-	      // Zweite Spalte: Artikel ID
-	      entryRow.addColumn(new Column(String.valueOf(le.getArticleId())));
-	      
-	   // Dritte Spalte: Menge
-	      entryRow.addColumn(new Column(String.valueOf(le.getAmount())));
-	      
-	   // Vierte Spalte: Käufer
-	      entryRow.addColumn(new Column(String.valueOf(le.getUserId())));
-	      
-	   // Fünfte Spalte: Händler
-	      entryRow.addColumn(new Column(String.valueOf(le.getStoreId())));
-	      
-	      
+		for (ListEntry le : listEntries) {
 
-	      // und schließlich die Zeile dem Report hinzufügen.
-	      result.addRow(entryRow);
-	    }
+			// Eine leere Zeile anlegen.
+			Row entryRow = new Row();
+
+			// Erste Spalte: ListEntry ID
+			entryRow.addColumn(new Column(String.valueOf(le.getId())));
+
+			// Zweite Spalte: Artikel ID
+			entryRow.addColumn(new Column(String.valueOf(le.getArticleId())));
+
+			// Dritte Spalte: Menge
+			entryRow.addColumn(new Column(String.valueOf(le.getAmount())));
+
+			// Vierte Spalte: Käufer
+			entryRow.addColumn(new Column(String.valueOf(le.getUserId())));
+
+			// Fünfte Spalte: Händler
+			entryRow.addColumn(new Column(String.valueOf(le.getStoreId())));
+
+			// und schließlich die Zeile dem Report hinzufügen.
+			result.addRow(entryRow);
+		}
 		return result;
 	}
-
-	
 
 }
