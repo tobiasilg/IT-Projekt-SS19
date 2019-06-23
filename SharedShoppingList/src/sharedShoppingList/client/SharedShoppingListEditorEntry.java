efa1adc029ee6e6f3757ce545896731dcccea224
nicolaifischbach <nicolai.fischbach@gmx.de>
2019-06-23 20:21:28 +0200

    test

--- SharedShoppingList/src/sharedShoppingList/client/SharedShoppingListEditorEntry.java
+++ SharedShoppingList/src/sharedShoppingList/client/SharedShoppingListEditorEntry.java
@@ -129,6 +129,10 @@ public class SharedShoppingListEditorEntry implements EntryPoint {
 			CurrentUser.u = u;
 		}
   	}
+  	
+  	public static class Unit {
+  		
+  	}
 }
 
 //public static class CurrentGroup {
