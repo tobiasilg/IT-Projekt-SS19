6818d47270ee4e38a668e5bd11ca605d70dcea43
patrick <treiber_patrick@web.de>
2019-06-28 14:04:43 +0200

    delete setGroupId and setGroupName

--- SharedShoppingList/src/sharedShoppingList/shared/bo/Group.java
+++ SharedShoppingList/src/sharedShoppingList/shared/bo/Group.java
@@ -1,37 +1,18 @@
 package sharedShoppingList.shared.bo;
 
-import java.sql.Timestamp;
-
 /**
- * Diese Klasse erweitert die allgemeine BusinessObject-Klasse um nutzerspezifische Funktionen und Parameter.
+ * Diese Klasse erweitert die allgemeine BusinessObject-Klasse um
+ * nutzerspezifische Funktionen und Parameter.
+ * 
  * @author Tobias Ilg, Leon Seiz
  */
 
-public class Group extends BusinessObject{
-	
+public class Group extends BusinessObject {
+
 	private static final long serialVersionUID = 1L;
-	
-	private int groupid;
-	private String groupname;
-	
+
 	public Group() {
-		super();	
-	}
-	
-	public int getGroupid() {
-		return groupid;
+		super();
 	}
 
-	public void setGroupid(int groupid) {
-		this.groupid = groupid;
-	}
-
-	public String getGroupname() {
-		return groupname;
-	}
-
-	public void setGroupname(String groupname) {
-		this.groupname = groupname;
-	}	
-	
 }
